743a8f5da6d7079fb395381c5a6c0c78cffd993d
Stig Otnes Kolstad <stig@stigok.com>
2018-05-15 15:14:36 +0200

    Fix news update script

--- .deployment/bin/download-facebook-news.sh
+++ .deployment/bin/download-facebook-news.sh
@@ -10,7 +10,5 @@ access_token="$(head -n 1 < $ACCESS_TOKEN_FILE)"
 dest=$1
 
 curl -sSfo "$dest.tmp" "https://graph.facebook.com/v2.10/hausmania/posts?fields=created_time%2Cdescription%2Cfrom%2Clink%2Cmessage%2Cname%2Cpermalink_url%2Cstatus_type%2Ctype%2Cpicture%2Cfull_picture&limit=10&access_token=$access_token"
-json_pp -json_opt utf8,pretty -f json -t json < $dest.tmp > /dev/null
+json_pp -json_opt utf8,pretty -f json -t json < $dest.tmp | tee $dest
 
-# If successful, copy to target destination
-cp $dest.tmp $dest
